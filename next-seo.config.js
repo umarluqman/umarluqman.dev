aa0ed6514dff6afae5900eaf1220d37e3b376f5d
Umar Luqman <>
2020-06-12 23:13:25 +0800

    Add og image of the site

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,6 +11,8 @@ const SEO = {
     url: "https://umarluqman.dev",
     title,
     description,
+    image:
+      "https://images.prismic.io/blog-next/2e099607-29dd-4621-baea-50e0c07e1614_Screenshot+at+Jun+12+23-06-03.png?auto=compress,format",
   },
   twitter: {
     handle: "@umarlqmn",
