230e72521035dee5ba71b4b1ce68ab3e1fbda6b7
Umar Luqman <>
2020-10-29 09:55:29 +0800

    Added on-page seo for now page

--- src/pages/now.js
+++ src/pages/now.js
@@ -1,4 +1,5 @@
 import { Flex, Heading, Icon, Text, useColorMode } from "@chakra-ui/core";
+import { NextSeo } from "next-seo";
 import React from "react";
 
 const BulletPoint = ({ children, ...stylingProps }) => {
@@ -16,40 +17,67 @@ const NowNowNow = () => {
   const smallTextColor = { light: "gray.700", dark: "gray.300" };
   const textColor = { light: "black", dark: "white" };
   return (
-    <Flex
-      direction="column"
-      align="flex-start"
-      w="full"
-      maxW={700}
-      color={textColor[colorMode]}
-    >
-      <Heading mb={8}>What I'm up to now?</Heading>
-      <BulletPoint mb={4}>Living in Cyberjaya, Malaysia.</BulletPoint>
-      <BulletPoint mb={4}>Coding with Next.js on my day job.</BulletPoint>{" "}
-      <BulletPoint mb={4}>
-        Into headless CMS and headless E-commerce.
-      </BulletPoint>
-      <BulletPoint mb={3}>
-        Trying to be fit and live a healthy lifestyle.
-      </BulletPoint>
-      <Text ml={8} mb={2}>
-        I get sweat almost everyday, learning to cook through my try and error
-        series.
-      </Text>
-      <Text ml={8}>
-        For cardio, I do jumprope. Learned to jumprope during Covid restricted
-        movement.
-      </Text>
-      <Text
-        ml={8}
-        as="i"
-        mt={8}
-        fontSize="sm"
-        color={smallTextColor[colorMode]}
+    <>
+      <NextSeo
+        title="Now"
+        description="What's I'm up to now?"
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url:
+                "https://images.prismic.io/shortcase/cbc84946-61be-4daa-a62f-941f0c814970_NOW.png?auto=compress,format",
+              width: 568,
+              height: 280,
+              alt: "",
+            },
+          ],
+          site_name: "Umar Luqman Digital Garden",
+        }}
+        twitter={{
+          handle: "@umarlqmn",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Flex
+        direction="column"
+        align="flex-start"
+        w="full"
+        maxW={700}
+        color={textColor[colorMode]}
       >
-        Last updated on October 2020
-      </Text>
-    </Flex>
+        <Heading mb={8}>What I'm up to now?</Heading>
+        <BulletPoint mb={4}>Living in Cyberjaya, Malaysia.</BulletPoint>
+        <BulletPoint mb={4}>
+          Coding with Next.js on my day job.
+        </BulletPoint>{" "}
+        <BulletPoint mb={4}>
+          Into headless CMS and headless E-commerce.
+        </BulletPoint>
+        <BulletPoint mb={3}>
+          Trying to be fit and live a healthy lifestyle.
+        </BulletPoint>
+        <Text ml={8} mb={2}>
+          I get sweat almost everyday, learning to cook through my try and error
+          series.
+        </Text>
+        <Text ml={8}>
+          For cardio, I do jumprope. Learned to jumprope during Covid restricted
+          movement.
+        </Text>
+        <Text
+          ml={8}
+          as="i"
+          mt={8}
+          fontSize="sm"
+          color={smallTextColor[colorMode]}
+        >
+          Last updated on October 2020
+        </Text>
+      </Flex>
+    </>
   );
 };
 
