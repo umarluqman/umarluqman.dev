a3e94dd9bcebd2991c0c2f0ad785e21863dce091
Umar Luqman <>
2020-10-30 21:55:10 +0800

    Lower excerpt font size

---
+++ src/screens/Blog/PostList.jsx
@@ -0,0 +1,164 @@
+/**@jsx jsx */
+import {
+  Badge,
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useColorMode,
+} from "@chakra-ui/core";
+import { jsx } from "@emotion/core";
+import dayjs from "dayjs";
+import Router from "next/router";
+import Header from "../../components/Header";
+import { headingColor, textColor } from "../../styles/colors";
+
+export default function Blog({ allPosts }) {
+  const { colorMode } = useColorMode();
+
+  const bgColor = { light: "gray.50", dark: "gray.800" };
+
+  const handleNav = (slug) => () => {
+    Router.push(`/blog/[slug]`, `/blog/${slug}`);
+  };
+
+  return (
+    <Flex
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      backgroundColor={bgColor[colorMode]}
+      pb={80}
+      width="full"
+    >
+      <Box maxWidth={810} w="full" as="main" p={6}>
+        <Header colorMode={colorMode}>Blog</Header>
+        <Text color={textColor[colorMode]}>
+          This blog is dedicated to share anything that I found interesting.
+        </Text>
+
+        <Stack spacing={6} py={8}>
+          {allPosts.map(({ node, readingTime }) => {
+            const {
+              title,
+              excerpt,
+              cover_image = "",
+              date,
+              category,
+              _meta: { uid },
+            } = node;
+
+            const badgeTextColor = {
+              light: `${category?.badge_color}.400`,
+              dark: `${category?.badge_color}.100`,
+            };
+            const badgeBgColor = {
+              light: `${category?.badge_color}.100`,
+              dark: `${category?.badge_color}.700`,
+            };
+
+            return (
+              <React.Fragment key={uid}>
+                <Flex
+                  flexWrap="wrap"
+                  onClick={handleNav(uid)}
+                  css={{ "&:hover": { cursor: "pointer" } }}
+                  align="center"
+                >
+                  <Image
+                    src={cover_image?.url}
+                    alt={cover_image?.alt}
+                    minW={300}
+                    maxW={{ sm: 367, md: 327 }}
+                    w="full"
+                    h={220}
+                    objectFit="cover"
+                    mb={{ base: 8, md: 0 }}
+                    borderRadius={8}
+                    boxShadow="0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);"
+                  />
+
+                  <Flex
+                    direction="column"
+                    p={{ sm: 0, md: 4 }}
+                    pl={{ sm: 0, md: 8 }}
+                    pt={0}
+                    justifyContent="space-between"
+                    flex={1}
+                  >
+                    <Box>
+                      <Badge
+                        mb={2}
+                        borderRadius={99}
+                        px={2}
+                        fontWeight={500}
+                        textTransform="capitalize"
+                        letterSpacing="wider"
+                        textColor={badgeTextColor[colorMode]}
+                        bg={badgeBgColor[colorMode]}
+                      >
+                        {category?.name}
+                      </Badge>
+                      <Heading
+                        as="h2"
+                        size={"lg"}
+                        color={headingColor[colorMode]}
+                        mb={2}
+                      >
+                        {title[0]?.text}
+                      </Heading>
+                      <Flex align="center">
+                        <Text
+                          fontWeight={400}
+                          textTransform="uppercase"
+                          color={textColor[colorMode]}
+                          fontSize="sm"
+                          mr={2}
+                        >
+                          {dayjs(date).format("DD MMMM YYYY")}
+                        </Text>
+                        <Box
+                          h="4px"
+                          w="4px"
+                          backgroundColor="gray.400"
+                          borderRadius="50%"
+                          display="inline-block"
+                          mr={2}
+                        ></Box>
+                        <Text
+                          fontWeight={400}
+                          textTransform="uppercase"
+                          color={textColor[colorMode]}
+                          fontSize="sm"
+                        >
+                          {readingTime?.text}
+                        </Text>
+                      </Flex>
+                      <Text
+                        mt={4}
+                        lineHeight={2}
+                        color={textColor[colorMode]}
+                        fontSize="sm"
+                        fontFamily="Inter"
+                      >
+                        {excerpt}
+                      </Text>
+                    </Box>
+                  </Flex>
+                </Flex>
+                {colorMode === "light" && (
+                  <Divider my={8} borderColor="gray.300" />
+                )}
+              </React.Fragment>
+            );
+          })}
+        </Stack>
+      </Box>
+    </Flex>
+  );
+}
+
+Blog.propTypes = {};
